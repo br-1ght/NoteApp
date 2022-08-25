ac19879d8478e5be9cff03d15581e0f9e086bf98
br-1ght <fcw2k16@gmail.com>
2022-08-25 19:40:55 +0800

    small change

--- src/main/java/com/br1ght/Main.java
+++ src/main/java/com/br1ght/Main.java
@@ -1,11 +1,9 @@
 package com.br1ght;
 
-
 import com.br1ght.App.NoteApp;
 
 public class Main {
     public static void main(String[] args) {
-        // new NoteApp();
         new NoteApp();
     }
 }
